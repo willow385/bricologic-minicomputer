1e0888454615db05bfadef71367a36acca5bbb08
Dante Falzone <dantefalzone0@protonmail.com>
2023-05-06 21:33:12 -0500

    Wrote display controller class and implemented demo animation

--- display_controller/display_controller.ino
+++ display_controller/display_controller.ino
@@ -1,45 +1,98 @@
-enum class Row {
-  // r0 represents the top row, and r5 represents the bottom row.
-  r0, r1, r2, r3, r4, r5
-};
-
-enum class Column {
-  // c0 represents the leftmost column, and c4 represents the rightmost.
-  c0, c1, c2, c3, c4
-};
-
-class DisplayController {
-  // Pins A0 through A4 correspond to columns; pins 0 through 5 correspond to rows.
-private:
-  boolean pinStates[30];
-public:
-  DisplayController() {
-    for (unsigned int i = 0; i < 30; i++) {
-      this->pinStates[i] = false;
-    }
-    
-  }
-};
-
-void setup() {
-  for (unsigned int pin = 0; pin < 4; pin++) {
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, LOW);
-  }
+enum class LedState : boolean { Dark = LOW, Bright = HIGH };
+LedState operator!(LedState state) {
+  return (state == LedState::Dark) ? LedState::Bright : LedState::Dark;
 }
 
-// display a spinner animation on the four LEDs
+constexpr uint8_t RowCount = 6;
+constexpr uint8_t ColumnCount = 5;
+
+class DisplayController {
+private:
+  LedState frameBuffer[30];
+public:
+  DisplayController(void) {
+    for (uint8_t i = 0; i < 30; i++) {
+      this->frameBuffer[i] = LedState::Dark;
+    }
+    // Pins A0 through A4 correspond to columns; pins 0 through 5 correspond to rows.
+    for (auto pin = A0; pin < A5; pin++) {
+      pinMode(pin, OUTPUT);
+      /* The column pins connect to the negative terminals of the LEDs, so setting them HIGH
+       * will turn them OFF. */
+      digitalWrite(pin, HIGH);
+    }
+    for (auto pin = 0; pin < 5; pin++) {
+      pinMode(pin, OUTPUT);
+      digitalWrite(pin, LOW);
+    }
+  }
+
+  void setLedState(const uint8_t col, const uint8_t row, LedState state) {
+    // The constant 5 here means the number of columns.
+    this->frameBuffer[col + row * 5] = state;
+  }
+
+  LedState getLedState(const uint8_t col, const uint8_t row) const {
+    return this->frameBuffer[col + row * 5];
+  }
+
+  void refresh(const unsigned int delayTimeMs) {
+    for (unsigned int elapsedTime = 0; elapsedTime < delayTimeMs; elapsedTime += 10) {
+      for (uint8_t row = 0; row < 5; row++) {
+        digitalWrite(row, HIGH);
+        for (uint8_t col = 0; col < 6; col++) {
+          digitalWrite(A0 + col, (boolean)!this->getLedState(col, row));
+        }
+        /* For the red rows, we wait 4 milliseconds; for the green and blue rows,
+         * we wait 1 millisecond. This helps balance out the brightnesses of the
+         * different colors.
+         */
+        if (row == 0 || row == 3) {
+          delay(4);
+        } else {
+          delay(1);
+        }
+        digitalWrite(row, LOW);
+      }
+    }
+  }
+
+  // This method will turn every pixel in the framebuffer dark.
+  void clearFrameBuffer(void) {
+    for (uint8_t i = 0; i < 30; i++) {
+      this->frameBuffer[i] = LedState::Dark;
+    }
+  }
+
+  // This method will invert every pixel in the framebuffer, making a negative image.
+  void negateFrameBuffer(void) {
+    for (uint8_t i = 0; i < 30; i++) {
+      this->frameBuffer[i] = !this->frameBuffer[i];
+    }
+  }
+
+  // This method will set a dark LED to bright and a bright one to dark.
+  void negateLedState(const uint8_t col, const uint8_t row) {
+    this->frameBuffer[col + row * 5] = !this->frameBuffer[col + row * 5];
+  }
+};
+
+
+DisplayController disp;
+uint8_t col, row;
+void setup() {
+  col = 0;
+  row = 0;
+}
+
 void loop() {
-  setLight(Row::Top,    Column::Right, LOW );
-  setLight(Row::Top,    Column::Left,  HIGH);
-  delay(100);
-  setLight(Row::Top,    Column::Left,  LOW );
-  setLight(Row::Bottom, Column::Left,  HIGH);
-  delay(100);
-  setLight(Row::Bottom, Column::Left,  LOW );
-  setLight(Row::Bottom, Column::Right, HIGH);
-  delay(100);
-  setLight(Row::Bottom, Column::Right, LOW );
-  setLight(Row::Top,    Column::Right, HIGH);
-  delay(100);
+  disp.setLedState(col, row, LedState::Bright);
+  disp.refresh(100);
+  disp.setLedState(col, row, LedState::Dark);
+  if (col == 4) {
+    row = !row;
+    col = 0;
+  } else {
+    col++;
+  }
 }
